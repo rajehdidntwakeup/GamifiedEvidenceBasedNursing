57d968e30f93f9d5f61a2a2b813905a5e1a3ebc5
Rajehdidntwakeup <rajeh3010@gmail.com>
2026-02-28 11:07:05 +0100

    Initialize Spring Boot project with Maven support including configurations for .gitignore, .gitattributes, and IntelliJ IDEA.

---
+++ src/main/java/bswe/gamifiedevidencebasednursing/GamifiedEvidenceBasedNursingApplication.java
@@ -0,0 +1,13 @@
+package bswe.gamifiedevidencebasednursing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamifiedEvidenceBasedNursingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GamifiedEvidenceBasedNursingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/bswe/gamifiedevidencebasednursing/GamifiedEvidenceBasedNursingApplicationTests.java
@@ -0,0 +1,13 @@
+package bswe.gamifiedevidencebasednursing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamifiedEvidenceBasedNursingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
